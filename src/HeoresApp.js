256d86adeb0b60f5363267bbc558d1fd59e0f6c5
Kevin Rangel <krangel107@gmail.com>
2022-01-09 21:56:08 -0500

    first commit

---
+++ src/HeoresApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeoresApp = () => {
+    return (
+        <div>
+            <h1>Heroes App</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HeoresApp } from './HeoresApp';
+
+ReactDOM.render(
+  <HeoresApp />,
+  document.getElementById('root')
+);
+
+
+
