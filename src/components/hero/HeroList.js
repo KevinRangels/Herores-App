22f4229c14af9647193ee06752277bcb0bb4bd7d
Kevin Rangel <krangel107@gmail.com>
2022-02-06 11:08:18 -0500

    useMemo in HeroList and add Animation

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,10 +1,12 @@
+import { useMemo } from 'react';
 import { getHerosByPublisher } from '../../selectors/getHerosByPublisher';
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHerosByPublisher(publisher);
+  const heroes = useMemo(() => getHerosByPublisher(publisher), [publisher]);
+
   return (
-    <div className="row rows-cols-1 row-cols-md-3 g-3">
+    <div className="row rows-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn">
       {heroes.map((hero) => (
         <HeroCard key={hero.id} hero={hero} />
       ))}
